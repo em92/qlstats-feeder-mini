817c57cf13916244d08e799081dd4f61b6c587a1
hbeham <horst@beham.biz>
2015-12-25 00:27:45 +0100

    - reformatted code
    - mark matches which are created while reprocessing as not-processed
    - only update ratings of players which were actually changed
    - added status code for JSON file missing (so that it doesn't get reprocessed)

--- rerun-glicko.node.js
+++ rerun-glicko.node.js
@@ -8,29 +8,31 @@
 
 
 // TODO: turn this into command line args
-var gametype = "ffa";
-var rateEachSingleMatch = true;
-
+var gametype = "ca";
 var mode = "incremental";
 
+var rateEachSingleMatch = true;
 if (mode == "full") {
   var resetRating = true;
   var updateDatabase = true;
   var printResult = false;
   var onlyProcessMatchesBefore = null;
-} else if (mode == "part1") {
+}
+else if (mode == "part1") {
   // calculate data up to a certain date to test incremental updates
   var resetRating = true;
   var updateDatabase = true;
   var printResult = true;
-  var onlyProcessMatchesBefore = new Date(Date.UTC(2015, 12 - 1, 1)); 
-} else if (mode == "part2") {
+  var onlyProcessMatchesBefore = new Date(Date.UTC(2015, 12 - 1, 1));
+}
+else if (mode == "part2") {
   // incrementally calculate updates for unrated matches
   var resetRating = false;
   var updateDatabase = false;
   var printResult = true;
   var onlyProcessMatchesBefore = null;
-} else if (mode == "incremental") {
+}
+else if (mode == "incremental") {
   // incrementally calculate updates for unrated matches
   var resetRating = false;
   var updateDatabase = true;
@@ -45,6 +47,7 @@ var g2 = new glicko.Glicko({ rating: 1500, rd: 350, c: Math.sqrt((Math.pow(350, 
 var _config;
 var strategy;
 var playersBySteamId = {};
+var _lastProcessedMatchStartDt;
 
 // values for DB column games.g2_status
 const
@@ -54,7 +57,8 @@ const
   ERR_ROUND_OR_TIMELIMIT = 3,
   ERR_BOTMATCH = 4,
   ERR_TEAMTIMEDIFF = 5,
-  ERR_MINPLAYERS = 6;
+  ERR_MINPLAYERS = 6,
+  ERR_DATAFILEMISSING = 7;
 
 function main() {
   _config = JSON.parse(fs.readFileSync(__dirname + "/cfg.json"));
@@ -99,20 +103,20 @@ function createGameTypeStrategy(gametype) {
     "ft": 8
   }
   var ValidateMatchForGametype = {
-    "duel": function (json) { return json.matchStats.GAME_LENGTH >= 10 * 60 - 5 || json.matchStats.EXIT_MSG.indexOf("forfeited") >= 0 },  
-    "ffa": function (json) { return json.matchStats.FRAG_LIMIT >= 50 },
-    "ca": function (json) { return Math.max(json.matchStats.TSCORE0, json.matchStats.TSCORE1) >= 10 /* old JSONS have no ROUND_LIMIT */ },
-    "tdm": function (json) { return Math.max(json.matchStats.TSCORE0, json.matchStats.TSCORE1) >= 100 || json.matchStats.GAME_LENGTH >= 15 * 10 },
-    "ctf": function (json) { return Math.max(json.matchStats.TSCORE0, json.matchStats.TSCORE1) >= 8 || json.matchStats.GAME_LENGTH >= 15 * 10 },
-    "ft": function (json) { return Math.max(json.matchStats.TSCORE0, json.matchStats.TSCORE1) >= 8 /* old JSONS have no ROUND_LIMIT */ }
+    "duel": function(json) { return json.matchStats.GAME_LENGTH >= 10 * 60 - 5 || json.matchStats.EXIT_MSG.indexOf("forfeited") >= 0 },
+    "ffa": function(json) { return json.matchStats.FRAG_LIMIT >= 50 },
+    "ca": function(json) { return Math.max(json.matchStats.TSCORE0, json.matchStats.TSCORE1) >= 10 /* old JSONS have no ROUND_LIMIT */ },
+    "tdm": function(json) { return Math.max(json.matchStats.TSCORE0, json.matchStats.TSCORE1) >= 100 || json.matchStats.GAME_LENGTH >= 15 * 10 },
+    "ctf": function(json) { return Math.max(json.matchStats.TSCORE0, json.matchStats.TSCORE1) >= 8 || json.matchStats.GAME_LENGTH >= 15 * 10 },
+    "ft": function(json) { return Math.max(json.matchStats.TSCORE0, json.matchStats.TSCORE1) >= 8 /* old JSONS have no ROUND_LIMIT */ }
   }
   var IsDrawForGametype = {
-    "duel": function (a, b) { return false; },
-    "ffa": function (a, b) { return Math.abs(a - b) <= 5; },
-    "ca": function (a, b) { return Math.abs(a - b) <= 2 },
-    "tdm": function (a, b) { return a / b <= 1.1 && b / a <= 1.1 },
-    "ctf": function (a, b) { return a / b <= 1.1 && b / a <= 1.1 },
-    "ft": function (a, b) { return Math.abs(a - b) <= 5; }
+    "duel": function() { return false; },
+    "ffa": function(a, b) { return Math.abs(a - b) <= 5; },
+    "ca": function(a, b) { return Math.abs(a - b) <= 2 },
+    "tdm": function(a, b) { return a / b <= 1.1 && b / a <= 1.1 },
+    "ctf": function(a, b) { return a / b <= 1.1 && b / a <= 1.1 },
+    "ft": function(a, b) { return Math.abs(a - b) <= 5; }
   }
 
   return {
@@ -126,7 +130,7 @@ function createGameTypeStrategy(gametype) {
 
 function dbConnect() {
   var defConnect = Q.defer();
-  pg.connect(_config.webapi.database, function (err, cli, release) {
+  pg.connect(_config.webapi.database, function(err, cli, release) {
     if (err)
       defConnect.reject(new Error(err));
     else {
@@ -143,8 +147,8 @@ function resetRatingsInDb(cli) {
 
   return Q
     .ninvoke(cli, "query", "update player_elos set g2_games=0, g2_r=0, g2_rd=0, g2_dt=null where game_type_cd=$1", [gametype])
-    .then(function () { return Q.ninvoke(cli, "query", "update player_game_stats pgs set g2_score=null, g2_delta_r=null, g2_delta_rd=null from games g where pgs.game_id=g.game_id and g.game_type_cd=$1", [gametype]) })
-    .then(function() { return Q.ninvoke(cli, "query", "update games set g2_status=$2 where game_type_cd=$1", [gametype, ERR_NOTRATEDYET]) });
+    .then(function() { return Q.ninvoke(cli, "query", "update player_game_stats pgs set g2_score=null, g2_delta_r=null, g2_delta_rd=null from games g where pgs.game_id=g.game_id and g.game_type_cd=$1", [gametype]) })
+    .then(function() { return Q.ninvoke(cli, "query", "update games set g2_status=$2 where game_type_cd=$1 and g2_status<>$3", [gametype, ERR_NOTRATEDYET, ERR_DATAFILEMISSING]) });
 }
 
 function loadPlayers(cli) {
@@ -162,6 +166,7 @@ function loadPlayers(cli) {
       result.rows.forEach(function(row) {
         var player = getOrAddPlayer(row.player_id, row.hashkey, row.nick, row.g2_r, row.g2_rd, glickoPeriod(row.g2_dt));
         player.games = row.g2_games;
+        player.mustSave = false;
       });
     });
 }
@@ -169,10 +174,10 @@ function loadPlayers(cli) {
 function getMatchIds(cli) {
   var cond = resetRating ? "" : " and g2_status=0";
   cond += " and (" + (onlyProcessMatchesBefore ? 1 : 0) + "=0 or start_dt<$1)";
-  
-  return Q.ninvoke(cli, "query", 
+
+  return Q.ninvoke(cli, "query",
       "select match_id, start_dt, game_id from games"
-      + " where game_type_cd='" + gametype + "' and mod in ('" + strategy.validFactories.join("','") + "')" + cond 
+      + " where game_type_cd='" + gametype + "' and mod in ('" + strategy.validFactories.join("','") + "')" + cond
       + " order by start_dt", [onlyProcessMatchesBefore || new Date()])
     .then(function(result) {
       return result.rows.map(function(row) { return { game_id: row.game_id, date: row.start_dt, match_id: row.match_id }; });
@@ -206,7 +211,6 @@ function processMatch(cli, matchId, date, gameId, ratingPeriod) {
   var subfolders = [];
   for (var i = 0; i < 3; i++)
     subfolders.push(getDateFolder(date, deltas[i]));
-  subfolders.push("omega/");
 
   var file = null;
   for (var i = 0; i < subfolders.length; i++) {
@@ -215,22 +219,22 @@ function processMatch(cli, matchId, date, gameId, ratingPeriod) {
       var stat = fs.statSync(file);
       if (stat && stat.isFile())
         break;
-      file = null;
-    } 
+    }
     catch (err) {
     }
+    file = null;
   }
 
   if (file) {
     return processFile(cli, gameId, file, ratingPeriod)
-      .catch(function (err) {
+      .catch(function(err) {
         console.log("Failed to process " + file + ": " + err);
         return false;
       });
   }
 
   console.log("json.gz not found: " + matchId);
-  return false;
+  return setGameStatus(cli, gameId, ERR_DATAFILEMISSING).then(Q(false));
 }
 
 function getDateFolder(date, deltaDays) {
@@ -250,27 +254,26 @@ function getDateFolder(date, deltaDays) {
 }
 
 function processFile(cli, gameId, file, ratingPeriod) {
-  return extractDataFromJson(file)
-    .then(function (result) {    
+  return extractDataFromJsonFile(file)
+    .then(function(result) {
       // store a status code why the game was not rated
       if (typeof (result) === "number")
-        return setGameStatus(result).then(Q(false));
-             
+        return setGameStatus(cli, gameId, result).then(Q(false));
+
       var playerRanking = result;
       var players = [];
       for (var i = 0; i < playerRanking.length; i++) {
         var r1 = playerRanking[i];
         var p1 = playersBySteamId[r1.id];
         p1.score = Math.round(r1.score);
+        p1.mustSave = true;
         players.push(p1);
         ++p1.games;
         if (r1.win)
           ++p1.wins;
-        p1.rating.oldR = p1.rating.getRating();
-        p1.rating.oldRd = p1.rating.getRd();
 
-        if (gameId == 863)
-          console.log(p1.name + ": r=" + p1.rating.oldR + ", rd=" + p1.rating.oldRd + ", g=" + p1.games + ", s=" + p1.score);
+        //if (gameId == 863)
+        //  console.log(p1.name + ": r=" + p1.rating.__oldR + ", rd=" + p1.rating.__oldRd + ", g=" + p1.games + ", s=" + p1.score);
 
         for (var j = i + 1; j < playerRanking.length; j++) {
           var r2 = playerRanking[j];
@@ -280,29 +283,29 @@ function processFile(cli, gameId, file, ratingPeriod) {
           g2.addResult(p1.rating, p2.rating, result);
         }
       }
-    
+
       if (rateEachSingleMatch)
         g2.calculatePlayersRatings(ratingPeriod);
 
       return (rateEachSingleMatch ? savePlayerGameRatingChange(players) : Q())
-        .then(function() { return setGameStatus(ERR_OK) })
+        .then(function() { return setGameStatus(cli, gameId, ERR_OK) })
         .then(Q(true));
     });
 
   function savePlayerGameRatingChange(players) {
-    return players.reduce(function (chain, p) {
-      return chain.then(function () {
+    return players.reduce(function(chain, p) {
+      return chain.then(function() {
         return getPlayerId(cli, p)
-          .then(function (pid) {
-            if (gameId == 863)
-              console.log(p.name + ": r=" + p.rating.getRating() + ", rd=" + p.rating.getRd());
+          .then(function(pid) {
+            //if (gameId == 863)
+            //  console.log(p.name + ": r=" + p.rating.getRating() + ", rd=" + p.rating.getRd());
 
             if (!updateDatabase)
               return Q();
 
-            var val = [gameId, pid, p.score, p.rating.getRating() - p.rating.oldR, p.rating.getRd() - p.rating.oldRd];
+            var val = [gameId, pid, p.score, p.rating.getRating() - p.rating.__oldR, p.rating.getRd() - p.rating.__oldRd];
             return Q.ninvoke(cli, "query", { name: "pgs_upd", text: "update player_game_stats set g2_score=$3, g2_delta_r=$4, g2_delta_rd=$5 where game_id=$1 and player_id=$2", values: val })
-              .then(function (result) {
+              .then(function(result) {
                 if (result.rowCount == 0)
                   console.log("player_game_stats not found: gid=" + gameId + ", pid=" + pid);
                 return Q();
@@ -311,96 +314,98 @@ function processFile(cli, gameId, file, ratingPeriod) {
       });
     }, Q());
   }
-
-  function setGameStatus(status) {
-    if (!updateDatabase)
-      return Q();
-    return Q.ninvoke(cli, "query", { name: "game_upd", text: "update games set g2_status=$2 where game_id=$1", values: [gameId, status] });
-  }
-
 }
 
-function extractDataFromJson(path) {
+function setGameStatus(cli, gameId, status) {
+  if (!updateDatabase)
+    return Q();
+  return Q.ninvoke(cli, "query", { name: "game_upd", text: "update games set g2_status=$2 where game_id=$1", values: [gameId, status] });
+}
+
+function extractDataFromJsonFile(path) {
   return Q
     .nfcall(fs.readFile, path)
     .then(function(data) { return Q.nfcall(zlib.gunzip, data); })
-    .then(function(json) {
-      var raw = JSON.parse(json);
+    .then(function(json) { return extractDataFromGameObject(JSON.parse(json)); });
+}
 
-      if (raw.matchStats.ABORTED) return ERR_ABORTED;
-      if (!strategy.validateGame(raw)) return ERR_ROUND_OR_TIMELIMIT;
+function extractDataFromGameObject(game) {
+  _lastProcessedMatchStartDt = game.gameEndTimestamp;
 
-      // aggregate total time, damage and score of player during a match (could have been switching teams)
-      var playerData = {}
-      var botmatch = false;
-      var timeRed = 0, timeBlue = 0, isTeamGame;
-      aggregateTimeAndScorePerPlayer();
+  if (game.matchStats.ABORTED) return ERR_ABORTED;
+  if (!strategy.validateGame(game)) return ERR_ROUND_OR_TIMELIMIT;
 
-      if (botmatch)
-        return ERR_BOTMATCH;
-      if (timeBlue != 0 && Math.abs(timeRed - timeBlue) > strategy.maxTeamTimeDiff)
-        return ERR_TEAMTIMEDIFF;
+  // aggregate total time, damage and score of player during a match (could have been switching teams)
+  var playerData = {}
+  var botmatch = false;
+  var timeRed = 0, timeBlue = 0, isTeamGame;
+  aggregateTimeAndScorePerPlayer();
 
-      var players = calculatePlayerRanking();
-      if (players.length < strategy.minPlayers)
-        return ERR_MINPLAYERS;
-      return players;
+  if (botmatch)
+    return ERR_BOTMATCH;
+  if (timeBlue != 0 && Math.abs(timeRed - timeBlue) > strategy.maxTeamTimeDiff)
+    return ERR_TEAMTIMEDIFF;
 
-      function aggregateTimeAndScorePerPlayer() {
-        raw.playerStats.forEach(function(p) {
-          botmatch |= p.STEAM_ID == "0";
-          if (p.WARMUP || botmatch) // p.ABORTED must be counted for team switchers
-            return;
+  var players = calculatePlayerRanking();
+  if (players.length < strategy.minPlayers)
+    return ERR_MINPLAYERS;
+  return players;
 
-          var pd = playerData[p.STEAM_ID];
-          if (!pd) {
-            pd = { id: p.STEAM_ID, name: p.NAME, timeRed: 0, timeBlue: 0, score: 0, k: 0, d: 0, dg: 0, dt: 0, win: false };
-            playerData[p.STEAM_ID] = pd;
-          }
+  function aggregateTimeAndScorePerPlayer() {
+    game.playerStats.forEach(function(p) {
+      botmatch |= p.STEAM_ID == "0";
+      if (p.WARMUP || botmatch) // p.ABORTED must be counted for team switchers
+        return;
 
-          var time = Math.min(p.PLAY_TIME, raw.matchStats.GAME_LENGTH); // pauses and whatever QL bugs can cause excessive PLAY_TIME
-          if (p.TEAM == 2) {
-            timeBlue += time;
-            pd.timeBlue += time;
-          } else {
-            timeRed += time;
-            pd.timeRed += time;
-          }
-          pd.score += p.SCORE;
-          pd.dg += p.DAMAGE.DEALT;
-          pd.dt += p.DAMAGE.TAKEN;
-          pd.k += p.KILLS;
-          pd.d += p.DEATHS;
-          if (p.RANK == 1)
-            pd.win = true;
-          isTeamGame |= p.hasOwnProperty("TEAM");
-        });
+      var pd = playerData[p.STEAM_ID];
+      if (!pd) {
+        pd = { id: p.STEAM_ID, name: p.NAME, timeRed: 0, timeBlue: 0, score: 0, k: 0, d: 0, dg: 0, dt: 0, win: false };
+        playerData[p.STEAM_ID] = pd;
       }
 
-      function calculatePlayerRanking() {
-        var players = [];
-        for (var steamId in playerData) {
-          if (!playerData.hasOwnProperty(steamId)) continue;
-          var pd = playerData[steamId];
-          if (pd.timeRed + pd.timeBlue < raw.matchStats.GAME_LENGTH / 2) // minumum 50% participation
-            continue;
-          if (pd.dg < 500 || pd.dt / pd.dg >= 10.0) // skip AFK players
-            continue;
-
-          getOrAddPlayer(null, pd.id, pd.name).played = true;
-
-          if (isTeamGame) {
-            var winningTeam = raw.matchStats.TSCORE0 > raw.matchStats.TSCORE1 ? -1 : raw.matchStats.TSCORE0 == raw.matchStats.TSCORE1 ? 0 : +1;
-            var playerTeam = pd.timeRed >= pd.timeBlue ? -1 : +1;
-            pd.win = playerTeam == winningTeam;
-          }
-
-          var rankingScore = calcPlayerPerformance(pd, raw);
-          players.push({ id: pd.id, score: rankingScore, win: pd.win });
-        }
-        return players;
+      var time = Math.min(p.PLAY_TIME, game.matchStats.GAME_LENGTH); // pauses and whatever QL bugs can cause excessive PLAY_TIME
+      if (p.TEAM == 2) {
+        timeBlue += time;
+        pd.timeBlue += time;
       }
+      else {
+        timeRed += time;
+        pd.timeRed += time;
+      }
+      pd.score += p.SCORE;
+      pd.dg += p.DAMAGE.DEALT;
+      pd.dt += p.DAMAGE.TAKEN;
+      pd.k += p.KILLS;
+      pd.d += p.DEATHS;
+      if (p.RANK == 1)
+        pd.win = true;
+      isTeamGame |= p.hasOwnProperty("TEAM");
     });
+  }
+
+  function calculatePlayerRanking() {
+    var players = [];
+    for (var steamId in playerData) {
+      if (!playerData.hasOwnProperty(steamId)) continue;
+      var pd = playerData[steamId];
+      if (pd.timeRed + pd.timeBlue < game.matchStats.GAME_LENGTH / 2) // minumum 50% participation
+        continue;
+      if (pd.dg < 500 || pd.dt / pd.dg >= 10.0) // skip AFK players
+        continue;
+
+      getOrAddPlayer(null, pd.id, pd.name).played = true;
+
+      if (isTeamGame) {
+        var winningTeam = game.matchStats.TSCORE0 > game.matchStats.TSCORE1 ? -1 : game.matchStats.TSCORE0 == game.matchStats.TSCORE1 ? 0 : +1;
+        var playerTeam = pd.timeRed >= pd.timeBlue ? -1 : +1;
+        pd.win = playerTeam == winningTeam;
+      }
+
+      var rankingScore = calcPlayerPerformance(pd, game);
+      players.push({ id: pd.id, score: rankingScore, win: pd.win });
+    }
+    return players;
+  }
 }
 
 function getOrAddPlayer(playerId, steamId, name, rating, rd, period) {
@@ -418,7 +423,7 @@ function calcPlayerPerformance(p, raw) {
     return (p.dt == 0 ? 2 : Math.min(2, Math.max(0.5, p.dg / p.dt))) * (p.score + p.dg / 20) * timeFactor + (p.win ? 300 : 0);
 
   // TDM performance formula inspired by http://qlstats.info/about-ql-statistics.html
-  if (gametype == "tdm") 
+  if (gametype == "tdm")
     return ((p.k - p.d) * 5 + (p.dg - p.dt) / 100 * 4 + p.dg / 100 * 3) * timeFactor;
 
   if (gametype == "duel")
@@ -451,7 +456,7 @@ function saveResults(cli, players) {
   for (var steamId in players) {
     if (!players.hasOwnProperty(steamId)) continue;
     var player = players[steamId];
-    if (player.games)
+    if (player.games && player.mustSave)
       list.push(player);
   }
 
@@ -467,6 +472,14 @@ function saveResults(cli, players) {
           });
       });
     }, Q())
+    .then(function() {
+      // reprocessing could have taken quite a long time and new matches could have been added with incorrect ratings, so mark the new games as NOT_RATED_YET
+      if (resetRating) {
+        var val = [gametype, ERR_NOTRATEDYET, ERR_DATAFILEMISSING, new Date(_lastProcessedMatchStartDt)];
+        return Q.ninvoke(cli, "query", "update games set g2_status=$2 where game_type_cd=$1 and g2_status<>$3 and start_dt>$4", val);
+      }
+      return Q();
+    })
     .then(Q(players));
 }
 
@@ -475,7 +488,7 @@ function getPlayerId(cli, player) {
     return Q(player.pid);
 
   return Q.ninvoke(cli, "query", { name: "player_by_steamid", text: "select player_id from hashkeys where hashkey=$1", values: [player.id] })
-    .then(function (result) {
+    .then(function(result) {
       if (result.rows.length == 0) {
         console.log("no player with steam-id " + player.id);
         return null;
@@ -487,10 +500,10 @@ function getPlayerId(cli, player) {
 function printResults() {
   if (!printResult)
     return playersBySteamId;
-  
+
   // bring all RD values to today's date  
-  var allRatings = Object.keys(playersBySteamId).map(function (key) { return playersBySteamId[key].rating; });
-  g2.setPeriod(glickoPeriod(new Date()), allRatings);
+  //var allRatings = Object.keys(playersBySteamId).map(function (key) { return playersBySteamId[key].rating; });
+  //g2.setPeriod(glickoPeriod(new Date()), allRatings);
 
   var players = [];
   for (var key in playersBySteamId) {
@@ -500,8 +513,8 @@ function printResults() {
     p.r1 = p.rating.getRating() - p.rating.getRd();
     players.push(p);
   }
-  players.sort(function (a, b) { return -(a.r1 - b.r1); });
-  players.forEach(function (p) {
+  players.sort(function(a, b) { return -(a.r1 - b.r1); });
+  players.forEach(function(p) {
     if (p.games < 5) return;
     console.log(p.name
       + ": r-rd=" + Math.round(p.r1)
