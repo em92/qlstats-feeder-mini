96d762b9bba6f90d9a5cc0753be04881cfab0322
hbeham <horst@beham.biz>
2016-01-18 12:37:32 +0100

    - added update_ranks.sql to xonstat repository and fixed cronjob scripts to use it
    - added hack to node-postgres from offsetting UTC timestamps a 2nd time

--- modules/utils.js
+++ modules/utils.js
@@ -4,8 +4,20 @@
 
 exports.dbConnect = dbConnect;
 
+var initialized = false;
+
 // connect to the DB
 function dbConnect(conn) {
+  // this hack prevents node-postgres from applying the local time offset to timestaps that are already in UTC 
+  // see: https://github.com/brianc/node-postgres/issues/429#issuecomment-24870258
+  if (!initialized) {
+    pg.types.setTypeParser(1114, function (stringValue) {
+      console.log(stringValue);
+      return new Date(Date.parse(stringValue + "+0000"));
+    });
+    initialized = true;
+  }
+
   var defConnect = Q.defer();
   pg.connect(conn, function (err, cli, release) {
     if (err)
