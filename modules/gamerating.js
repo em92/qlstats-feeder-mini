a35ac725da419fc40fd2cb0b6ee80eaede99abad
Eugene Molotov <eugene.molotov@yandex.ru>
2017-12-09 22:15:25 +0500

    modified CA perfomance calculation
    In clan arena, score for some reason does not equal to `damage_given / 100 + kills`. So `score - 0.75 * kills` does not equal to `damage_given / 100 + 0.25 * kills`

--- modules/gamerating.js
+++ modules/gamerating.js
@@ -585,7 +585,7 @@ function calcPlayerPerformance(p, raw) {
 
   // then use score/rounds for CA
   if (gametype == "ca")
-    return (p.score - 0.75*p.k) * timeFactor * (p.win ? 1.2 : 1.0);
+    return (p.dg / 100 + 0.25*p.k) * timeFactor * (p.win ? 1.2 : 1.0);
 
   if (gametype == "ft")
     return (p.dg / 100 + 0.5*(p.k - p.d) + 2*p.a) * timeFactor;
